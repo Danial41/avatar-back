cc69971ec25f796c7bd0c827d5e09ccb98c34805
Denis <prorutop@yandex.ru>
2021-06-13 14:44:54 +0300

    add description column to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_12_184122) do
+ActiveRecord::Schema.define(version: 2021_06_13_114310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2021_06_12_184122) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
